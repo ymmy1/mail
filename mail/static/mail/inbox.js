ade7d8308fc90dbba97ae930f6bb58e8d64276ad
ymmy1 <chem-pion@mail.ru>
2020-08-18 01:27:16 -0700

    email-page

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,7 +20,27 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').onsubmit = (e) => { 
+
+    e.preventDefault();
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        alert(result.message)
+        load_mailbox('inbox')
+    });
+  }
 }
+
 
 function load_mailbox(mailbox) {
   
@@ -41,10 +61,12 @@ function load_mailbox(mailbox) {
   .then(response => response.json())
   .then(emails => {
       // Print emails
-      console.log(emails);
-      console.log(emails.length);
       table = document.querySelector('#inbox_table');
       table.innerHTML = "<tr><th>ID</th><th>Sender</th><th>Subject</th><th>Body</th><th>Time</th></tr>";
+      if(mailbox == "sent")
+      {
+        table.innerHTML = "<tr><th>ID</th><th>Recipients</th><th>Subject</th><th>Body</th><th>Time</th></tr>";
+      }
       if (emails.length == 0)
       {
         table = document.querySelector('#emails-view');
@@ -56,30 +78,35 @@ function load_mailbox(mailbox) {
       {
         for (i = 0; i < emails.length; i++)
         {
-          console.log(emails[i].id)
           tr = document.createElement("tr");
-          tr.innerHTML = `<td> ${emails[i].id} </td><td> ${emails[i].sender} </td><td> ${emails[i].subject} </td><td> ${emails[i].body} </td><td> ${emails[i].timestamp} </td>`
+          tr.setAttribute('data-page', emails[i].id);
+          tr.setAttribute('class', 'email-link');
+          tr.addEventListener('click', function() {
+            fetch(`/emails/${this.dataset.page}`)
+              .then(response => response.json())
+              .then(email => {
+                var main = document.querySelector('#emails-view')
+                main.innerHTML = `
+                <div>
+                    <h4 class="email-title">${email.subject}</h4>
+                    <p class="email-from">From: ${email.sender}</p>
+                    <p class="email-from">To: ${email.recipients}</p>
+                    <p class="email-time">${email.timestamp}</p>
+                    <p class="email-body">${email.body}</p>
+                </div>
+                `
+              });
+          });
+          if(mailbox == "sent")
+          {
+            tr.innerHTML = `<td> ${emails[i].id} </td><td> ${emails[i].recipients} </td><td> ${emails[i].subject} </td><td> ${emails[i].body} </td><td> ${emails[i].timestamp} </td>`
+          }
+          else
+          {
+            tr.innerHTML = `<td> ${emails[i].id} </td><td> ${emails[i].sender} </td><td> ${emails[i].subject} </td><td> ${emails[i].body} </td><td> ${emails[i].timestamp} </td>`
+          }
           table.appendChild(tr);
         }
       }
-      
-      // ... do something else with emails ...
   });
-  // if (mailbox == 'sent')
-  // {
-  //   fetch('/emails', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //         recipients: 'ymmy@example.com',
-  //         subject: 'Meeting hour',
-  //         body: 'How about we meet tomorrow at 31pm?'
-  //     })
-  //   })
-  //   .then(response => response.json())
-  //   .then(result => {
-  //       // Print result
-  //       console.log(result);
-  //   });
-  // }
-  
 }
